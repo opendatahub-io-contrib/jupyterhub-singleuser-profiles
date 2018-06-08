b7be33e011627ae0391212b13d394960bab6302c
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-06-08 19:32:58 +0200

    Add initial implementation

---
+++ jupyterhub_singleuser_profiles/profiles.py
@@ -0,0 +1,42 @@
+import kubernetes
+import os
+import yaml
+
+
+class SingleuserProfiles(object):
+  def __init__(self):
+    self.profiles = {}
+    
+  def load_profiles(self, secret_name=None, filename=None, key_name="jupyterhub-singleuser-profiles.yaml"):
+    load_from_api = True
+    if filename and not secret_name:
+      load_from_api = False
+    if not secret_name:
+      secret_name = "jupyter-singleuser-profiles"
+
+    if load_from_api:
+      service_account_path = '/var/run/secrets/kubernetes.io/serviceaccount'
+
+      with open(os.path.join(service_account_path, 'namespace')) as fp:
+          namespace = fp.read().strip()
+
+      kubernetes.config.load_incluster_config()
+
+      api_client = kubernetes.client.CoreV1Api()
+
+      config_map = api_client.read_namespaced_config_map(secret_name, namespace)
+      self.profiles = yaml.load(config_map.data[key_name])["profiles"]
+    else:
+      with open(filename) as fp:
+        data = yaml.load(fp)
+        self.profiles = yaml.load(data["data"][key_name])["profiles"]
+
+  def get_profile_by_image(self, image):
+    for profile in self.profiles:
+      if profile.get("images") and len(profile.get("images")) > 0:
+        if image in profile.get("images"):
+          return profile
+
+    return None
+
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+def get_install_requires():
+    with open('requirements.txt', 'r') as requirements_file:
+        # TODO: respect hashes in requirements.txt file
+        res = requirements_file.readlines()
+        return [req.split(' ', maxsplit=1)[0] for req in res if req]
+
+
+def get_test_requires():
+  if os.path.exists('requirements-test.txt'):
+    with open('requirements-test.txt', 'r') as requirements_file:
+        res = requirements_file.readlines()
+        return [req.split(' ', maxsplit=1)[0] for req in res if req]
+  else:
+    return []
+
+
+class Test(TestCommand):
+    user_options = [
+        ('pytest-args=', 'a', "Arguments to pass into py.test")
+    ]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = ['--timeout=2', '--cov=./thoth', '--capture=no', '--verbose']
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        sys.exit(pytest.main(self.pytest_args))
+
+
+setup(
+    name='jupyterhub-singleuser-profiles',
+    version="0.0.1",
+    description='A tool to parse yaml configuration files used in singleuser JupyterHub servers',
+    long_description='A tool to parse yaml configuration files used in singleuser JupyterHub servers',
+    author='Vaclav Pavlin',
+    author_email='vasek@redhat.com',
+    license='GPLv3+',
+    packages=[
+        'jupyterhub_singleuser_profiles'
+    ],
+    zip_safe=False,
+    install_requires=get_install_requires(),
+    tests_require=get_test_requires(),
+    cmdclass={'test': Test},
+)
