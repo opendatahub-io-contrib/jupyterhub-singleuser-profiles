08401782018e5bac69e1e1d8030d8552f7acbd5a
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-06-11 15:15:05 +0200

    Add ability to merge multiple profiles (to support global/default) profile

--- jupyterhub_singleuser_profiles/profiles.py
+++ jupyterhub_singleuser_profiles/profiles.py
@@ -1,7 +1,9 @@
 import kubernetes
 import os
 import yaml
+import logging
 
+logger = logging.getLogger(__name__)
 
 class SingleuserProfiles(object):
   def __init__(self):
@@ -36,9 +38,37 @@ class SingleuserProfiles(object):
       if profile.get("images") and len(profile.get("images")) > 0:
         if image in profile.get("images"):
           if not user or not profile.get("users") or "*" in profile.get("users", []):
-            return profile
+            logger.info("Found profile for image %s, not filtering by user" % image)
+            yield profile
           if user in profile.get("users", []):
-            return profile
+            logger.info("Found profile '%s' for image %s, filtering by user %s" % (profile.get("name"), image, user))
+            yield profile
+      else:
+        yield profile
 
-    return {}
+    return iter(())
 
+  def get_merged_profile(self, image, user=None):
+    profile = self.get_profile_by_image(image, user)
+    res = self.empty_profile()
+    for p in profile:
+      res = self.merge_profiles(res, p)
+
+    return res
+
+  @classmethod
+  def empty_profile(self):
+    return {
+      "name": "",
+      "users": [],
+      "images": [],
+      "env": {}
+    }
+
+  @classmethod
+  def merge_profiles(self, profile1, profile2):
+    profile1["name"] =  ", ".join(filter(None, [profile1.get("name", ""), profile2.get("name", "")]))
+    profile1["images"] = list(set(profile1.get("images", []) + profile2.get("images", [])))
+    profile1["users"] = list(set(profile1.get("users", []) + profile2.get("users", [])))
+    profile1["env"] = {**profile1.get('env', {}), **profile2.get('env', {})}
+    return profile1
