e868a6600125fe72c23755f2792a03eb6475e8be
mroman-redhat <mroman@redhat.com>
2020-03-05 15:04:07 +0100

    Implemented kerberos resource structure to profiles and sizes.

--- jupyterhub_singleuser_profiles/sizes.py
+++ jupyterhub_singleuser_profiles/sizes.py
@@ -30,6 +30,14 @@ class Sizes(object):
             selected = ""
             if size['name'] == last_size:
                 selected = "selected=selected"
-            options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Memory: %s, CPU: %s)" % (size['name'], size['resources']['mem_limit'], size['resources']['cpu_limit'])))
+            if size['resources'].get('mem_limit') or size['resources'].get('cpu_limit'):
+                # Kept for backwards compatibility
+                options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Memory: %s, CPU: %s)" % (size['name'], size['resources']['mem_limit'], size['resources']['cpu_limit'])))
+            else:
+                req_mem = size['resources']['requests']['memory']
+                req_cpu = size['resources']['requests']['cpu']
+                lim_mem = size['resources']['limits']['memory']
+                lim_cpu = size['resources']['limits']['cpu']
+                options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Memory request: %s, CPU request: %s, Memory limit: %s, CPU limit: %s)" % (size['name'], req_mem, req_cpu, lim_mem, lim_cpu)))
 
         return template % "\n".join(options) if options else ""
