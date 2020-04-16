f3e5e9370fb9b3b54bc3141efe4ee4bb44384710
mroman-redhat <mroman@redhat.com>
2020-04-16 14:15:47 +0200

    Allowed blank resources

--- jupyterhub_singleuser_profiles/sizes.py
+++ jupyterhub_singleuser_profiles/sizes.py
@@ -30,6 +30,9 @@ class Sizes(object):
             selected = ""
             if size['name'] == last_size:
                 selected = "selected=selected"
+            if size['resources'] is None or ((size['resources'].get('limits') is None) and (size['resources'].get('requests') is None)): 
+                options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Cluster default values)" % (size['name'])))
+                continue
             if size['resources'].get('mem_limit') or size['resources'].get('cpu_limit'):
                 # Kept for backwards compatibility
                 options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Memory: %s, CPU: %s)" % (size['name'], size['resources']['mem_limit'], size['resources']['cpu_limit'])))
@@ -39,10 +42,19 @@ class Sizes(object):
                 elif size['resources'].get('limits') is None:
                     size['resources']['limits'] = size['resources']['requests']
 
-                req_mem = size['resources']['requests']['memory']
-                req_cpu = size['resources']['requests']['cpu']
-                lim_mem = size['resources']['limits']['memory']
-                lim_cpu = size['resources']['limits']['cpu']
+                req_mem = size['resources']['requests'].get('memory')
+                if req_mem is None:
+                    req_mem = "Default"
+                req_cpu = size['resources']['requests'].get('cpu')
+                if req_cpu is None:
+                    req_cpu = "Default"
+                lim_mem = size['resources']['limits'].get('memory')
+                if lim_mem is None:
+                    lim_mem = "Default"
+                lim_cpu = size['resources']['limits'].get('cpu')
+                if lim_cpu is None:
+                    lim_cpu = "Default"
+
                 options.append("<option value='%s' %s>%s</option>" % (size['name'], selected, "%s (Memory request: %s, CPU request: %s, Memory limit: %s, CPU limit: %s)" % (size['name'], req_mem, req_cpu, lim_mem, lim_cpu)))
 
         return template % "\n".join(options) if options else ""
