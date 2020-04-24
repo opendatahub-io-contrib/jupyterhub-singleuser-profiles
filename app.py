18b182e71e4c74ecf8ad082a3899b79b0510fadf
mroman-redhat <mroman@redhat.com>
2020-04-24 12:29:06 +0200

    changed app.py to tornado server

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import connexion
 def hello_world():
     return "Hello World!"
 
-app = connexion.App(__name__, specification_dir='.')
+app = connexion.App(__name__, specification_dir='.', server='tornado', options={'swagger_ui':True})
 app.add_api('swagger.yaml')
 app.run(port=8080)
