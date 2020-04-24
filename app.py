5499f06a50c1d4ab4c50d2ca47f325497166cf63
mroman-redhat <mroman@redhat.com>
2020-04-24 12:14:13 +0200

    Fixed swagger errors

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import connexion
 def hello_world():
     return "Hello World!"
 
-app = connexion.App(__name__, specification_dir='.', options={"swagger_ui": True})
+app = connexion.App(__name__, specification_dir='.')
 app.add_api('swagger.yaml')
 app.run(port=8080)
