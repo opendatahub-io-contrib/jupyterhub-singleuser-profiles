04fb8789669eca67d3a5ddfd47ce68c064e08d6e
mroman-redhat <mroman@redhat.com>
2021-08-23 09:31:57 +0200

    Add capacity check to sizes property

--- jupyterhub_singleuser_profiles/api/api.py
+++ jupyterhub_singleuser_profiles/api/api.py
@@ -82,6 +82,10 @@ def whoami(user):
 def get_instance(*args, **kwargs):
     return _PROFILES.get_instance()
 
+#@authenticated
+#def get_size_capacity(*args, **kwargs):
+#    return _PROFILES.get_size_capacity()
+
 @authenticated
 def get_user_cm(user):
     cm = _PROFILES.user.get(user['name'])
